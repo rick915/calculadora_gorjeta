19ea16d5c410a3abe0450939381ddba71d6ca071
rick915 <134823910+rick915@users.noreply.github.com>
2024-03-31 17:01:47 -0300

    quarto commit JS

--- calculadora_web/script.js
+++ calculadora_web/script.js
@@ -27,16 +27,26 @@ function recerberQuantidadePessoas(evento){
 
 const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
 botoesGorjeta.forEach(botao => {
-    botao.addEventListener("click", receberPorcentagemBotao)
+    botao.addEventListener("click", receberPorcentagem)
 })
 
-function receberPorcentagemBotao(evento) {
+function receberPorcentagem(evento) {
     botoesGorjeta.forEach(botao => {
         botao.classList.remove("botao-ativo")
-        
+
         if(botao.value === evento.target.value) {
             botao.classList.add("botao-ativo")
         }
     })
-    porcentagem = parseFloat(evento.target.value)/100
+
+    if(evento.target.value !== ""){
+        porcentagem = parseFloat(evento.target.value)/100
+    }else{
+        porcentagem =0
+    }
+
+    
 }
+
+const gorjetaInput =document.querySelector("#outra")
+gorjetaInput.addEventListener("input", receberPorcentagem)
